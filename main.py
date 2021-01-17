1aebc6e20ee4b4ee53cc445339d2cddac515ca09
oacdlr <54248409+oacdlr@users.noreply.github.com>
2021-01-17 17:55:47 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -9,9 +9,17 @@ def rutas(ciudades,inicio,fin):
 def listarrutas(opciones):
     for i in opciones:
         print(i)
+def suma(routes,opcion):
+    s=0
+    for i in range(len(opcion)-1):
+        print(opcion[i],"->",opcion[i+1],end='/')
+        print(routes[opcion[i]-1][opcion[i+1]-1])
+        s=s+routes[opcion[i]-1][opcion[i+1]-1]
+    return s
 def main():
     routes=((0,10,55,25,45),(10,0,20,25,40),(55,20,0,15,30),(25,25,15,0,50),(45,40,30,50,0))
     ciudades=(1,2,3,4,5)
+    print(suma(routes,ciudades))
     inicio=int(input("Nodo de inicio"))
     fin=int(input("Nodo de destino"))
     opciones=rutas(ciudades,inicio,fin)
